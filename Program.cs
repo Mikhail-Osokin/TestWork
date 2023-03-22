4780777c593d05e4bb6cb42dca52d84ab0084c09
Mikhail <un.less.jk@gmail.com>
2023-03-22 22:03:36 +0300

    add program file solution

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿/*Программа из имеющегося массива строк формирует массив из строк, дилна которых меньше либо равна 3 символам. 
+Первоначальный массив вводится с клавиатуры.
+
+Например:
+
+["hello","2","world",":-/"] -> ["2",":-/"]
+*/
+
+Console.WriteLine("Введите строки через запятую");
+string [] strArrayA = Console.ReadLine().Split(',');
+for (int i = 0; i < strArrayA.Length; i++)
+{
+    if (strArrayA[i].Length <=3)
+    {
+        Console.Write($"{strArrayA[i] }");
+    }
+}
